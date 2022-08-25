ed99085bcb1e82b94a56891f95a9408ba67d1d14
levimendyk <levi3743@gmail.com>
2022-08-25 14:44:50 -0600

    Initial commit and README completed (need to add video tutorial link)

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# team-generator
+# Team-Generator
+
+## User Story
+
+<br>
+
+AS A manager
+I WANT to generate a webpage that displays my team's basic info
+SO THAT I have quick access to their emails and GitHub profiles
+
+# Acceptance Criteria
+
+GIVEN a command-line application that accepts user input
+<br>
+
+<ul>
+<li>WHEN I am prompted for my team members and their information</li>
+<li>THEN an HTML file is generated that displays a nicely formatted team roster based on user input</li>
+<li>WHEN I click on an email address in the HTML</li>
+<li>THEN my default email program opens and populates the TO field of the email with the address</li>
+<li>WHEN I click on the GitHub username</li>
+<li>THEN that GitHub profile opens in a new tab</li>
+<li>WHEN I start the application</li>
+<li>THEN I am prompted to enter the team manager's name, employee ID, email address, and office number</li>
+<li>WHEN I enter the team manager's name, employee ID, email address, and office number</li>
+<li>THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team</li>
+<li>WHEN I select the intern option</li>
+<li>THEN I am prompted to enter the intern's name, ID, email, and school, and I am taken back to the menu</li>
+<li>WHEN I decide to finish building my team</li>
+<li>THEN I exit the application, and the HTML is generated</li>
+</ul>
+
+[Video Tutorial](https://placeholder)
+
+Contribution guidelines for this project
+<img src="./assets/images/team-generator-capture.png"/>
---
+++ index.js
@@ -0,0 +1,68 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      type: "input",
+      message: "What is the the project title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What is the description?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What is the installation instructions?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "What is the usage informations?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "What are the contribution guidelines?",
+      name: "guidelines",
+    },
+    {
+      type: "input",
+      message: "What are the tests?",
+      name: "tests",
+    },
+    {
+      type: "list",
+      message: "What is the license you are using?",
+      name: "license",
+      choices: ["Apache", "MIT", "BOOST", "None"],
+    },
+    {
+      type: "input",
+      message: "What is the link to the GitHub profile?",
+      name: "link",
+    },
+    {
+      type: "input",
+      message:
+        "What is your email address to contact for additional questions?",
+      name: "email",
+    },
+  ])
+  .then((response) => {
+    // Use user feedback for... whatever!!
+    const readMeContent = generateReadMe.generateMarkdown(response);
+    fs.writeFile("README.md", readMeContent, (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.error(error);
+    }
+  });
