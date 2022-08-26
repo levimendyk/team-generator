373380b64ad1e526fd8c4a7ae4522668ded8cdee
levimendyk <levi3743@gmail.com>
2022-08-25 18:46:12 -0600

    created each js file and started to code in them

--- index.js
+++ index.js
@@ -1,73 +1,59 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHTML = request("./utils/generatehtml.js");
-const newDiv = document.createElement("div");
-const 
+const Employee = require("./utils/employee");
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-      type: "input",
-      message: "What is the the project title?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "What is the description?",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "What is the installation instructions?",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "What is the usage informations?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "What are the contribution guidelines?",
-      name: "guidelines",
-    },
-    {
-      type: "input",
-      message: "What are the tests?",
-      name: "tests",
-    },
-    {
-      type: "list",
-      message: "What is the license you are using?",
-      name: "license",
-      choices: ["Apache", "MIT", "BOOST", "None"],
-    },
-    {
-      type: "input",
-      message: "What is the link to the GitHub profile?",
-      name: "link",
-    },
-    {
-      type: "input",
-      message:
-        "What is your email address to contact for additional questions?",
-      name: "email",
-    },
-  ])
-  .then((response) => {
-    // Use user feedback for... whatever!!
-    const htmlContent = generateHTML.generateMarkdown(response);
-    fs.writeFile("index.html", htmlContent, (err) =>
-      err ? console.error(err) : console.log("Team Genarted Successfully!")
-    );
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-      console.error(error);
-    }
-  });
+const generateHTML = require("./utils/generatehtml.js");
+// const newDiv = document.createElement("div");
 
-  //Create a new div
+// Runs through all the individual functions
+function runApp() {
+  generateManager();
+}
+
+// Generate first prompt, creating a Team, first question is to about the manager of the team
+function generateManager() {
+  console.log("Build Your Team 👥");
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "manager-name",
+      },
+      {
+        type: "input",
+        message: "What is the team manager's id?",
+        name: "manager-id",
+      },
+      {
+        type: "input",
+        message: "What is the team manager's email?",
+        name: "manager-email",
+      },
+      {
+        type: "input",
+        message: "What is the team manager's office number?",
+        name: "manager-officenumber",
+      },
+    ])
+    .then((response) => {
+      // Use user feedback for... whatever!!
+      const htmlContent = generateHTML(response);
+      fs.writeFile("index.html", htmlContent, (err) =>
+        err ? console.error(err) : console.log("Team Genarted Successfully!")
+      );
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.error(error);
+      }
+    });
+}
+
+// Next display information asking about the other members of the team, employee, engineer, intern
+
+// Call the Logic to run app
+runApp();
