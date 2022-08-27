adcb55352b0c4140d274773b08e252d097905705
levimendyk <levi3743@gmail.com>
2022-08-26 19:52:07 -0600

    Added some Classes to each of the JS and extended the Employee Class to Manager, Intern, and Engineer

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const generateHTML = require("./utils/generatehtml.js");
 // Runs through all the individual functions
 function runApp() {
   generateManager();
+  generateEngineer();
+  generateIntern();
 }
 
 // Generate first prompt, creating a Team, first question is to about the manager of the team
@@ -53,7 +55,85 @@ function generateManager() {
     });
 }
 
-// Next display information asking about the other members of the team, employee, engineer, intern
+// Generate Engineer
+function generateEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team engineer's name?",
+        name: "engineer-name",
+      },
+      {
+        type: "input",
+        message: "What is the team engineer's id?",
+        name: "engineer-id",
+      },
+      {
+        type: "input",
+        message: "What is the team engineer's email?",
+        name: "engineer-email",
+      },
+      {
+        type: "input",
+        message: "What is the team engineer's GitHub?",
+        name: "engineer-github",
+      },
+    ])
+    .then((response) => {
+      // Use user feedback for... whatever!!
+      const htmlContent = generateHTML(response);
+      fs.writeFile("index.html", htmlContent, (err) =>
+        err ? console.error(err) : console.log("Team Genarted Successfully!")
+      );
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.error(error);
+      }
+    });
+}
+
+// Generate Intern
+function generateIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team intern's name?",
+        name: "intern-name",
+      },
+      {
+        type: "input",
+        message: "What is the team intern's id?",
+        name: "intern-id",
+      },
+      {
+        type: "input",
+        message: "What is the team intern's email?",
+        name: "intern-email",
+      },
+      {
+        type: "input",
+        message: "What is the team intern's school?",
+        name: "intern-school",
+      },
+    ])
+    .then((response) => {
+      // Use user feedback for... whatever!!
+      const htmlContent = generateHTML(response);
+      fs.writeFile("index.html", htmlContent, (err) =>
+        err ? console.error(err) : console.log("Team Genarted Successfully!")
+      );
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.error(error);
+      }
+    });
+}
 
 // Call the Logic to run app
 runApp();
--- utils/manager.js
+++ utils/manager.js
@@ -1,3 +1,11 @@
-class Manager {
-    constructor()
+const Employee = require("./employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+  getRole() {
+    return "Manager";
+  }
 }
